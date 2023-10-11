fab7ab0eb91bb9af2342ed6161daddfd95c0cfcd
manishbsta <bistamanish681@gmail.com>
2023-10-11 11:27:45 +0545

    set up splash screen

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,22 +1,34 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as SplashScreen from 'expo-splash-screen';
 import React, { useEffect } from 'react';
-import LoginScreen from '../screens/LoginScreen';
-import { useAppSelector } from '../store/hooks';
+import * as SplashScreen from 'expo-splash-screen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import MainStack from './MainStack';
+import LoginScreen from '../screens/LoginScreen';
+
 import { RootStackParams } from './_types';
+import { setToken } from '../store/slices/auth.slice';
+import { getItemFromStorage } from '../utils/expo-secure-store';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
 
 const { Screen, Navigator } = createNativeStackNavigator<RootStackParams>();
 const RootNavigation = () => {
+	const dispatch = useAppDispatch();
 	const { token } = useAppSelector((s) => s.auth);
 
 	useEffect(() => {
+		getDataFromStorage();
+
 		const timeout = setTimeout(() => {
 			SplashScreen.hideAsync();
 		}, 2500);
 
 		return () => clearTimeout(timeout);
 	}, []);
+
+	const getDataFromStorage = async () => {
+		const accessToken = await getItemFromStorage('token');
+		if (accessToken) dispatch(setToken(accessToken));
+	};
 
 	return (
 		<Navigator screenOptions={{ headerShown: false }}>
